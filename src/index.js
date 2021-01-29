77c1a094d7deb6a7b219dba842bcdd3a2ecfd76f
Peter Juma <peterjuma@users.noreply.github.com>
2021-01-30 01:00:34 +0300

    Update packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import $ from "jquery";
+import markdownitEmoji from "markdown-it-emoji";
+import markdownitTaskLists from "markdown-it-task-lists";
+import * as marked from "marked";
+import MarkdownIt from "markdown-it";
+import TurndownService from "turndown";
+import * as turndownPluginGfm from "turndown-plugin-gfm";
 
 import App from "./App";
+
+// Turndown
+var turndownService = new TurndownService()
+var gfm = turndownPluginGfm.gfm
+turndownService.use(gfm)
+
+// Markdown-It & markdownitEmoji
+var md = new MarkdownIt();
+md.use(markdownitEmoji);
+
+// Task List
+md.use(markdownitTaskLists)
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
@@ -10,3 +29,16 @@ ReactDOM.render(
   </StrictMode>,
   rootElement
 );
+
+var html = "<h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit :)</h2>"
+var mark = "# Prophet :)"
+
+var markdown = turndownService.turndown(html)
+const markdown2 = turndownService.turndown(marked(html));
+
+var turnhtml = md.render(mark);
+
+console.log(markdown);
+console.log(markdown2);
+console.log(turnhtml);
+console.log(marked(html));
