47f51ee945dd51d41ab3635a869424adffaed56b
Peter Juma <peterjuma@users.noreply.github.com>
2021-01-30 13:38:04 +0300

    Add fontawesome - CDN & Packages

--- src/index.js
+++ src/index.js
@@ -7,8 +7,32 @@ import * as marked from "marked";
 import MarkdownIt from "markdown-it";
 import TurndownService from "turndown";
 import * as turndownPluginGfm from "turndown-plugin-gfm";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as FontAwesome from '@fortawesome/fontawesome-free-solid';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faBeer, faCoffee, faWineGlassAlt, faStroopwafel, faAppleAlt, faCookie } from '@fortawesome/free-solid-svg-icons'
+library.add(
+  fab,
+  faBeer,
+  faCoffee,
+  faWineGlassAlt,
+  faStroopwafel,
+  faAppleAlt,
+  faCookie
+)
+//Icons.js
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 import App from "./App";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 // Turndown
 var turndownService = new TurndownService()
@@ -26,6 +50,36 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
+    <h1>
+      <FontAwesomeIcon icon={FontAwesome.faTrash} /><br></br>
+      <FontAwesomeIcon icon={FontAwesome.faWindowClose} /><br></br>
+      <FontAwesomeIcon icon={FontAwesome.faBars} /><br></br>
+      <FontAwesomeIcon icon={FontAwesome.faCheck} className="far fa-sm"/><br></br>
+      <h4><FontAwesomeIcon icon={faCoffee} className="fa fa-sm" /> This is Font Awesome Icon.</h4><br></br>
+      <h4><FontAwesomeIcon icon={faCoffee} className="fa fa-2x" /> This is Font Awesome Large Icon.</h4><br></br>
+
+      Beer <FontAwesomeIcon icon="beer" /><br></br>
+      Coffee: <FontAwesomeIcon icon="coffee" /><br></br>
+      Wine: <FontAwesomeIcon icon="wine-glass-alt" /><br></br>
+
+      Stroopwafel: <FontAwesomeIcon icon="stroopwafel" /><br></br>
+      Apple: <FontAwesomeIcon icon="apple-alt" /><br></br>
+      Cookie: <FontAwesomeIcon icon="cookie" /><br></br>
+
+      <ul class="fa-ul">
+        <li><i class="fa-li fa fa-check-square"></i>List icons</li>
+        <li><i class="fa-li fa fa-spinner fa-spin"></i>List icons</li>
+        <li><i class="fa-li fa fa-square"></i>List icons</li>
+      </ul>
+
+      <i class="fa fa-spinner fa-spin"></i>
+      <i class="fa fa-circle-o-notch fa-spin"></i>
+      <i class="fa fa-refresh fa-spin"></i>
+      <i class="fa fa-cog fa-spin"></i>
+      <i class="fa fa-spinner fa-pulse"></i>
+
+      <div><i className="fa fa-spinner fa-spin">no spinner but why</i></div><br></br>
+    </h1>
   </StrictMode>,
   rootElement
 );
@@ -41,4 +95,4 @@ var turnhtml = md.render(mark);
 console.log(markdown);
 console.log(markdown2);
 console.log(turnhtml);
-console.log(marked(html));
+console.log(FontAwesome.faPhone);
