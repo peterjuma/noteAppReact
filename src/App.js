eac216b49baae6471561f1ca637e61050693c69b
Peter Juma <peterjuma@users.noreply.github.com>
2021-02-06 13:48:37 +0300

    Init Frame

--- src/App.js
+++ src/App.js
@@ -1,12 +1,173 @@
-import React from "react";
 import "./styles.css";
 import "./github.css";
+import React from "react";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+        <div className="left">
+            <div className="nav-bar">
+                <div className="nav-left-icon">
+                    <button id="homeBtn" className="btn"><i className="fa fa-home fa-lg" aria-hidden="true"></i></button>
+                    <p>All Notes</p>
+                    <button id="addNoteBtn" className="btn"><i className="fa fa-plus fa-lg" aria-hidden="true"></i></button>
+                </div>
+            </div>
+            <div className="search-bar">
+                    <button className="searchButton"><i className="fa fa-search" aria-hidden="true"></i></button>
+                    <input type="search" placeholder="Search Notes" className="search-field" />
+            </div>
+
+            <div className="note-list">
+                <div className="note-list-item"><span>Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. </span></div>
+                <div className="note-list-item"><span>Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. </span></div>
+                <div className="note-list-item"><span>Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. </span></div>
+                <div className="note-list-item"><span>Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. </span></div>
+                <div className="note-list-item"><span>Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </span></div>
+                <div className="note-list-item"><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. END </span></div>
+            </div>
+        </div>
+            
+        <div className="right">
+            <div className="nav-bar">
+                <div className="nav-right-icon">
+                    <i className="fas fa-expand fa-lg btn" aria-hidden="true" style={{float: "left"}}></i>
+                    <i className="far fa-edit fa-lg btn" style={{float: "left"}}> </i>
+                    <i className="far fa-trash-alt fa-lg btn" aria-hidden="true" style={{float: "right"}}></i>
+                    <i className="far fa-copy fa-lg btn" style={{float: "right"}}></i>
+                    <i className="fas fa-download fa-lg btn" style={{float: "right"}}></i> 
+                </div>
+            </div> 
+            <div className="main">
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+                <p>
+                    Phasellus nec sem in justo pellentesque facilisis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce ac felis sit amet ligula pharetra condimentum.
+
+                    Donec sodales sagittis magna. Mauris sollicitudin fermentum libero. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Aenean commodo ligula eget dolor.
+
+                    Phasellus magna. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Morbi mollis tellus ac sapien. Curabitur turpis. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla.
+
+                    Cras non dolor. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse potenti. Nunc nonummy metus. In consectetuer turpis ut velit.
+
+                    Praesent nonummy mi in odio. Fusce a quam. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Cras dapibus.
+                </p>
+            </div>
+            
+        </div>
+        
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,17 +7,11 @@ import * as marked from "marked";
 import MarkdownIt from "markdown-it";
 import TurndownService from "turndown";
 import * as turndownPluginGfm from "turndown-plugin-gfm";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as FontAwesome from '@fortawesome/fontawesome-free-solid';
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { fas } from '@fortawesome/free-solid-svg-icons'
-// import { far } from '@fortawesome/free-regular-svg-icons'
-// import { fab } from '@fortawesome/free-brands-svg-icons'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faBeer, faCoffee, faWineGlassAlt, faStroopwafel, faAppleAlt, faCookie } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faBeer, faCoffee, faWineGlassAlt, faStroopwafel, faAppleAlt, faCookie } from '@fortawesome/free-solid-svg-icons';
 library.add(
   fab,
   faBeer,
@@ -27,12 +21,9 @@ library.add(
   faAppleAlt,
   faCookie
 )
-//Icons.js
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 
 import App from "./App";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import "./styles.css";
 
 // Turndown
 var turndownService = new TurndownService()
@@ -50,46 +41,14 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
-    <h1>
-      <FontAwesomeIcon icon={FontAwesome.faTrash} /><br></br>
-      <FontAwesomeIcon icon={FontAwesome.faWindowClose} /><br></br>
-      <FontAwesomeIcon icon={FontAwesome.faBars} /><br></br>
-      <FontAwesomeIcon icon={FontAwesome.faCheck} className="far fa-sm"/><br></br>
-      <h4><FontAwesomeIcon icon={faCoffee} className="fa fa-sm" /> This is Font Awesome Icon.</h4><br></br>
-      <h4><FontAwesomeIcon icon={faCoffee} className="fa fa-2x" /> This is Font Awesome Large Icon.</h4><br></br>
-
-      Beer <FontAwesomeIcon icon="beer" /><br></br>
-      Coffee: <FontAwesomeIcon icon="coffee" /><br></br>
-      Wine: <FontAwesomeIcon icon="wine-glass-alt" /><br></br>
-
-      Stroopwafel: <FontAwesomeIcon icon="stroopwafel" /><br></br>
-      Apple: <FontAwesomeIcon icon="apple-alt" /><br></br>
-      Cookie: <FontAwesomeIcon icon="cookie" /><br></br>
- 
-      <ul class="fa-ul">List of Icons: 
-        <li><i class="fa-li fa fa-check-square"></i>List icons</li>
-        <li><i class="fa-li fa fa-spinner fa-spin"></i>List icons</li>
-        <li><i class="fa-li fa fa-square"></i>List icons</li>
-      </ul>
-
-      <i class="fa fa-spinner fa-spin"></i><br></br>
-      <i class="fa fa-circle-o-notch fa-spin"></i><br></br>
-      <i class="fa fa-refresh fa-spin"></i><br></br>
-      <i class="fa fa-cog fa-spin"></i><br></br>
-      <i class="fa fa-spinner fa-pulse"></i><br></br>
-
-      <div><i className="fas fa-spinner fa-spin">no spinner but why</i></div><br></br>
-    </h1>
   </StrictMode>,
   rootElement
 );
 
-var html = "<h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit :)</h2>"
-var mark = "# Prophet :)"
-
+var html = "<h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit :)</h2>";
+var mark = "# Prophet :)";
 var markdown = turndownService.turndown(html)
 const markdown2 = turndownService.turndown(marked(html));
-
 var turnhtml = md.render(mark);
 
 console.log(markdown);
