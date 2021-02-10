7d716e2c6a099847e42b79ec09d9bccf2ab6ffce
peterjuma <peterjuma@users.noreply.github.com>
2021-02-11 00:18:06 +0300

    WIP: Save edited/Updated note

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import "./styles.css";
 import "./github.css";
 import React, { Component } from "react";
-import NavbarLeft from "./NavbarLeft"
-import NavbarRight from "./NavbarRight"
+import NavbarSidebar from "./NavbarSidebar"
+import NavbarMain from "./NavbarMain"
 import NoteList from "./NoteList"
 import NoteMain from "./NoteMain"
 import readmePath from './README.md'
 import NoteEditor from "./NoteEditor"
-
-// Generate random notes data 
-import { LoremIpsum } from "lorem-ipsum";
-const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-      max: 7,
-      min: 3
-    },
-    wordsPerSentence: {
-      max: 8,
-      min: 4
-    }
-  });
-
-var rand = Math.floor(Math.random() * 10);
-const titleCase = (phrase) => {
-  return phrase[0].toUpperCase() + phrase.substring(1);
-};
-
-var note = {};
-const notesData = [];
-for (var i = 0; i < 15; i++) {
-    note = {
-        noteid: Math.random().toString(16).slice(2),
-        title: titleCase(lorem.generateWords(rand)),
-        body: lorem.generateParagraphs(rand),
-        created_at: Math.round(Date.now()).toString(),
-        updated_at: ""
-      };
-      notesData.push(note);
-}
-// End of Lorem Ipsum Note
+import notesData from "./LoremNotesum"
 
 // Load First Note
 if (notesData.length > 0) {
@@ -47,6 +16,8 @@ if (notesData.length > 0) {
   var body = notesData[0].body
 }
 
+
+
 class App extends Component {
     constructor() {
         super();
@@ -54,12 +25,15 @@ class App extends Component {
           noteid: noteid || '',
           notetitle: title || '',
           notebody: body || '',
-          activepage: "viewnote" // editnote // previewnote // viewnote // newnote
+          activepage: "viewnote", // editnote // previewnote // viewnote 
+          action:'', // addnote // updatenote
+          allnotes: notesData
         };
         this.handleNoteListItemClick = this.handleNoteListItemClick.bind(this)
         this.handleClickHomeBtn = this.handleClickHomeBtn.bind(this)
         this.handleNoteListItemMouseOver = this.handleNoteListItemMouseOver.bind(this)
         this.handleEditNote = this.handleEditNote.bind(this)
+        this.handleSaveNote = this.handleSaveNote.bind(this)
       }
       componentDidMount() {
         if(notesData.length == 0){      
@@ -74,7 +48,8 @@ class App extends Component {
               noteid: note.noteid,
               notetitle: note.title,
               notebody: note.body,
-              activepage: "viewnote" 
+              activepage: "viewnote",
+              action:''
             }
           )
         // Toggle note-clikced class 
@@ -110,43 +85,75 @@ class App extends Component {
                 noteid: '00000000',
                 notetitle: readmetitle,
                 notebody: readmebody,
-                activepage: "viewnote"
+                activepage: "viewnote",
+                action:''
               }
           )
         })
       }
 
       handleEditNote = (e, note) => {
-        // console.log(note)
         this.setState(
           {
             noteid: note.noteid,
             notetitle: e.target.id === "notetitle" ? e.target.value : note.notetitle,
             notebody: e.target.id === "notebody" ? e.target.value : note.notebody,
-            activepage: "editnote"
+            activepage: "editnote",
+            action: e.target.dataset.action
           }
         )
       }
 
+      handleSaveNote(e, note) {
+        console.log(e.target.dataset);
+        this.setState((prevState) => {
+          const updatedNotes = prevState.allnotes.map((noteitem) => {
+              if (noteitem.noteid === note.noteid) {
+                  noteitem.title = document.getElementById('notetitle').value
+                  noteitem.body = document.getElementById('notebody').value
+                  noteitem.activepage = "viewnote"
+                  noteitem.action = e.target.dataset.action
+                  console.log(noteitem);
+              }
+              return noteitem;
+          });
+          return {
+            allnotes: updatedNotes
+          };
+        });
+        // console.log(this.state.allnotes);
+
+        this.setState(
+          { 
+          noteid: note.noteid,
+          notetitle: document.getElementById('notetitle').value,
+          notebody: document.getElementById('notebody').value,
+          activepage: "viewnote",
+          action: note.action
+        })
+
+      }
+
       render() {
-        const noteListItems = notesData.map((note) => (
-          <NoteList key={note.noteid} note={note} handleClick={this.handleNoteListItemClick} handleMouseOver={this.handleNoteListItemMouseOver} handleMouseOut={this.handleNoteListItemMouseOut}/>
+        const noteListItems = this.state.allnotes.map((note) => (
+          <NoteList key={note.noteid} note={note} handleClick={this.handleNoteListItemClick} 
+          handleMouseOver={this.handleNoteListItemMouseOver} handleMouseOut={this.handleNoteListItemMouseOut}/>
         ));
 
         let ActivePage, RightNavbar;
         if(this.state.activepage === "viewnote"){
-          RightNavbar = <NavbarRight display={true} notesData={this.state} handleEditNote={this.handleEditNote}/>
+          RightNavbar = <NavbarMain display={true} notesData={this.state} handleEditNote={this.handleEditNote}/>
           ActivePage = <NoteMain notesData={this.state}/>
         } 
         if (this.state.activepage === "editnote"){
-          RightNavbar = <NavbarRight display={false}/>
-          ActivePage = <NoteEditor editNoteData={this.state} handleEditNote={this.handleEditNote}/>
+          RightNavbar = <NavbarMain display={false}/>
+          ActivePage = <NoteEditor editNoteData={this.state} handleEditNote={this.handleEditNote} handleSaveNote={this.handleSaveNote}/>
         }   
 
         return (
           <div className="container">
               <div className="left">   
-                  <NavbarLeft handleClickHomeBtn={this.handleClickHomeBtn} handleEditNote={this.handleEditNote}/>
+                  <NavbarSidebar handleClickHomeBtn={this.handleClickHomeBtn} handleEditNote={this.handleEditNote}/>
                   <div className="note-list">
                       {noteListItems}
                   </div>
--- src/NoteEditor.js
+++ src/NoteEditor.js
@@ -2,37 +2,38 @@ import React from "react";
 
 function NoteEditor(props) {
     var note = props.editNoteData;
-  return (
-      <div className="main-editor">
+    console.log(note);
+    return (
+        <div className="main-editor">
             <div className="title-header">
                 <input name="title" type="text" id="notetitle" value={note.notetitle} placeholder="Title" autoComplete="off" onChange={((e) => props.handleEditNote(e, note))} />
             </div>
-          <div className="md-editor-tools" id="mdtools">
-              <button className="md-buttons" id="btnBold"><i className="fas fa-bold btn"></i></button>
-              <button className="md-buttons" id="btnItalic"><i className="fas fa-italic btn"></i></button>
-              <button className="md-buttons" id="btnHeading"><i className="fas fa-heading btn"></i></button>
-              <button className="md-buttons" id="btnLink"><i className="fas fa-link btn"></i></button>
-              <button className="md-buttons" id="btnOList"><i className="fas fa-list-ol btn"></i></button>
-              <button className="md-buttons" id="btnUList"><i className="fas fa-list btn"></i></button>
-              <button className="md-buttons" id="btnQuote"><i className="fas fa-quote-left btn"></i></button>
-              <button className="md-buttons" id="btnImage"><i className="far fa-image fa-lg btn"></i></button>
-              <button className="md-buttons" id="btnCode"><i className="fas fa-terminal btn"></i></button>
-              <button className="md-buttons" id="btnCodeBlock"><i className="fas fa-code btn"></i></button>
-              <button className="md-buttons" id="btnTask"><i className="fas fa-check-square btn"></i></button>
-              <button className="md-buttons" id="btnTable"><i className="fas fa-table btn"></i></button>
-              <button className="md-buttons" id="btnStrike"><i className="fas fa-strikethrough btn"></i></button>
-              <button className="md-buttons" id="previewBtn"><i className="fas fa-eye btn"></i></button>
-              <button className="md-buttons" id="splitScreen"><i className="fas fa-columns btn"></i></button>
-              <div className="">
-                    <button className="md-buttons"><i className="fas fa-window-close btn"></i></button>
-                    <button className="md-buttons" id="splitScreen"><i className="fas fa-save btn"></i></button>
+            <div className="md-editor-tools" id="mdtools">
+                <i className="fas fa-bold btn"></i>
+                <i className="fas fa-italic btn"></i>
+                <i className="fas fa-heading btn"></i>
+                <i className="fas fa-link btn"></i>
+                <i className="fas fa-list-ol btn"></i>
+                <i className="fas fa-list btn"></i>
+                <i className="fas fa-quote-left btn"></i>
+                <i className="far fa-image fa-lg btn"></i>
+                <i className="fas fa-terminal btn"></i>
+                <i className="fas fa-code btn"></i>
+                <i className="fas fa-check-square btn"></i>
+                <i className="fas fa-table btn"></i>
+                <i className="fas fa-strikethrough btn"></i>
+                <i className="fas fa-eye btn"></i>
+                <i className="fas fa-columns btn"></i>
+                <div className="">
+                    <i className="fas fa-window-close btn"></i>
+                    <i className="fas fa-save btn" onClick={((e) => props.handleSaveNote(e, note))} data-action={note.action}></i>
                 </div>
-          </div>
-          <div className="md-txtarea">
-              <textarea placeholder="Note" id="notebody" value={note.notebody} onChange={((e) => props.handleEditNote(e, note))} />
-          </div> 
-      </div>
-  )
+            </div>
+            <div className="md-txtarea">
+                <textarea placeholder="Note" id="notebody" value={note.notebody} onChange={((e) => props.handleEditNote(e, note))} />
+            </div> 
+        </div>
+    )
 }
 
 export default NoteEditor;
